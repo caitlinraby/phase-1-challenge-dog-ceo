e7aaefb8f2730f9648919b7adeab2f5c3fb15e8a
Caitlin Raby <caitlinraby@gmail.com>
2021-05-26 15:37:58 -0500

    commit tree 9d5f5af1583ec54a11d5182156e4fe608a82a643

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
+
+document.addEventListener("DOMContentLoaded", fetchContent)
+
+function fetchContent(e){
+    
+    fetch (imgUrl)
+    .then (res => res.json()) //parses the response as json
+    .then (data => addDogPics(data.message))//the data you're fetching
+
+    
+}
+
+function addDogPics(data){
+
+const imageContainer = document.querySelector('div#dog-image-container')
+data.forEach( (url) => {
+    const newImg = document.createElement('img')
+    newImg.src = url
+    imageContainer.append(newImg)
+})
+
+}
